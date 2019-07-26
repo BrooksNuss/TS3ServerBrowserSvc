d2425a66776d3a55bd89b98ac88e306f40294ebb
BrooksNuss <bnussjr@gmail.com>
2019-07-25 20:32:24 -0400

    First successful RTC implementation. Moved to HTTPS

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import 'ts3-nodejs-library';
-import * as socketIo from 'socket.io';
 import {ts3Config} from './config';
 import { setupTSListeners, registerTSEvents } from './socket/socketSetup';
 import bodyParser = require('body-parser');
+const socketIo = require('socket.io');
 const TeamSpeak3 = require('ts3-nodejs-library');
 const cors = require('cors');
 const app = express();
 const expressPort = 8080;
 const socketPort = 8081;
+const fs = require('fs');
+const https = require('https');
+const privateKey = fs.readFileSync('../server.key', 'utf8');
+const cert = fs.readFileSync('../server.crt', 'utf8');
+const httpsCredentials = {key: privateKey, cert};
 const ts3: TeamSpeak3 = new TeamSpeak3(ts3Config);
-// const http = require('http').Server(app);
-// const socketServer = require('socket.io')(http);
 let ts3Ready = false;
 const socketConnections: Array<SocketIO.Socket> = [];
 
@@ -20,11 +23,11 @@ app.use(bodyParser.json());
 
 app.use('/api/v1', require('./routes/base'));
 
-// start the Express server
-const server = app.listen( expressPort, () => {
-    console.log( `server started at http://localhost:${ expressPort }` );
-} );
-const socketServer = socketIo.default(server);
+const httpsServer = https.createServer(httpsCredentials, app);
+httpsServer.listen(expressPort, () => {
+    console.log( `server started at https://localhost:${ expressPort }` );
+})
+const socketServer = socketIo(httpsServer);
 
 ts3.on('ready', async () => {
     try {
@@ -42,11 +45,11 @@ ts3.on('error', (err) => {
     console.log(err);
 });
 
-socketServer.on('connection', (socket) => {
+socketServer.on('connection', (socket: any) => {
     socketConnections.push(socket);
     console.log('socket connected:');
     socket.send('Websocket connection ready');
-    socket.on('message', msg => {
+    socket.on('message', (msg: any) => {
         console.log(msg);
     });
 });
--- src/rtc/rtcApp.ts
+++ src/rtc/rtcApp.ts
@@ -1,19 +1,10 @@
 const WebRtcConnectionManager = require('./connections/webrtcconnectionmanager.js');
-const {RtcAudioSink} = require('wrtc').nonstandard;
 
 const connectionManager = WebRtcConnectionManager.create({beforeOffer});
 
 function beforeOffer(peerConnection: RTCPeerConnection) {
-    // peerConnection.ontrack = (track: MediaTrack) => {
-    //     console.log(track);
-    // };
-    const audioTransceiver = peerConnection.addTransceiver('audio');
-    audioTransceiver.sender.replaceTrack(audioTransceiver.receiver.track);
-    let sink = new RtcAudioSink(audioTransceiver.receiver.track);
-    sink.onData = (data: any) => {
-        console.log(data);
-    }
-    return audioTransceiver.receiver.track;
+    let transceiver = peerConnection.addTransceiver('audio');
+    return Promise.all([transceiver.sender.replaceTrack(transceiver.receiver.track)]);
 }
 
 export {connectionManager};
